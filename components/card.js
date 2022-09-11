9dc6970afbea0ce27c1783da8d5eab5c13c6ef33
GxbrielTxrres <gtorres2204@outlook.com>
2022-09-10 20:30:44 -0500

    updated the main page and added some new data

---
+++ components/card.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Card.module.css";
+import cls from "classnames";
+
+const Card = (props) => {
+  return (
+    <div>
+      <Link href={props.href}>
+        <a className={styles.cardLink}>
+          <div className={cls("glass", styles.container)}>
+            <div className={styles.cardHeaderWrapper}>
+              <h2 className={styles.cardHeader}>{props.name}</h2>
+            </div>
+            <div className={styles.cardImageWrapper}>
+              <Image
+                className={styles.cardImage}
+                src={props.imgUrl}
+                width={260}
+                height={160}
+              />
+            </div>
+          </div>
+        </a>
+      </Link>
+
+      {/* name of the coffe shop */}
+    </div>
+  );
+};
+
+export default Card;
---
+++ components/heading.js
@@ -0,0 +1,5 @@
+const Heading = () => {
+  return <div>Hello</div>;
+};
+
+export default Heading;
